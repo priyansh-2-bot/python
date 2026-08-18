dd24fe2a90e3069e01a543d72c8ea521462951da
Priyansh Singla <singlapriyansh2k6@gmail.com>
2026-08-18 10:12:29 +0530

    Add files via upload

---
+++ Store multiplication tables in different files from 2 to 20.py
@@ -0,0 +1,155 @@
+i=1
+for i in range(1,11):
+    a=2
+    str2=f"{a} X {i} = {a*i}"
+f=open("Table_of_2.txt","w")
+for i in range(1,11):
+    a=2
+    str2=f"{a} X {i} = {a*i}"
+    f.write(str2)
+    f.write(", ")
+f.close()
+
+f=open("Table_of_3.txt","w")
+for i in range(1,11):
+    a=3
+    str3=f"{a} X {i} = {a*i}"
+    f.write(str3)
+    f.write(", ")
+f.close()
+
+f=open("Table_of_4.txt","w")
+for i in range(1,11):
+    a=4
+    str4=f"{a} X {i} = {a*i}"
+    f.write(str4)
+    f.write(", ")
+f.close()
+
+f=open("Table_of_5.txt","w")
+for i in range(1,11):
+    a=5
+    str5=f"{a} X {i} = {a*i}"
+    f.write(str5)
+    f.write(", ")
+f.close()
+
+f=open("Table_of_6.txt","w")
+for i in range(1,11):
+    a=6
+    str6=f"{a} X {i} = {a*i}"
+    f.write(str6)
+    f.write(", ")
+f.close()
+
+f=open("Table_of_7.txt","w")
+for i in range(1,11):
+    a=7
+    str7=f"{a} X {i} = {a*i}"
+    f.write(str7)
+    f.write(", ")
+f.close()
+
+f=open("Table_of_8.txt","w")
+for i in range(1,11):
+    a=8
+    str8=f"{a} X {i} = {a*i}"
+    f.write(str8)
+    f.write(", ")
+f.close()
+
+f=open("Table_of_9.txt","w")
+for i in range(1,11):
+    a=9
+    str9=f"{a} X {i} = {a*i}"
+    f.write(str9)
+    f.write(", ")
+f.close()
+
+f=open("Table_of_10.txt","w")
+for i in range(1,11):
+    a=10
+    str10=f"{a} X {i} = {a*i}"
+    f.write(str10)
+    f.write(", ")
+f.close()
+
+f=open("Table_of_11.txt","w")
+for i in range(1,11):
+    a=11
+    str11=f"{a} X {i} = {a*i}"
+    f.write(str11)
+    f.write(", ")
+f.close()
+
+f=open("Table_of_12.txt","w")
+for i in range(1,11):
+    a=12
+    str12=f"{a} X {i} = {a*i}"
+    f.write(str12)
+    f.write(", ")
+f.close()
+
+f=open("Table_of_13.txt","w")
+for i in range(1,11):
+    a=13
+    str13=f"{a} X {i} = {a*i}"
+    f.write(str13)
+    f.write(", ")
+f.close()
+
+f=open("Table_of_14.txt","w")
+for i in range(1,11):
+    a=14
+    str14=f"{a} X {i} = {a*i}"
+    f.write(str14)
+    f.write(", ")
+f.close()
+
+f=open("Table_of_15.txt","w")
+for i in range(1,11):
+    a=15
+    str15=f"{a} X {i} = {a*i}"
+    f.write(str15)
+    f.write(", ")
+f.close()
+
+f=open("Table_of_16.txt","w")
+for i in range(1,11):
+    a=16
+    str16=f"{a} X {i} = {a*i}"
+    f.write(str16)
+    f.write(", ")
+f.close()
+
+f=open("Table_of_17.txt","w")
+for i in range(1,11):
+    a=17
+    str17=f"{a} X {i} = {a*i}"
+    f.write(str17)
+    f.write(", ")
+f.close()
+
+f=open("Table_of_18.txt","w")
+for i in range(1,11):
+    a=18
+    str18=f"{a} X {i} = {a*i}"
+    f.write(str18)
+    f.write(", ")
+f.close()
+
+f=open("Table_of_19.txt","w")
+for i in range(1,11):
+    a=19
+    str19=f"{a} X {i} = {a*i}"
+    f.write(str19)
+    f.write(", ")
+f.close()
+
+f=open("Table_of_20.txt","w")
+for i in range(1,11):
+    a=20
+    str20=f"{a} X {i} = {a*i}"
+    f.write(str20)
+    f.write(", ")
+f.close()
